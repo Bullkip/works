f2ec7e0c66d85d9464829a381ebbe0e6b43c2771
Bullkip <01sinister@mail.com>
2019-06-06 00:48:06 +0300

    fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,12 @@ $(document).ready(function () {
         speed: 400,
         slidesToShow: 4,
         slidesToScroll: 1,
+        initialSlide:0,
         responsive: [{
                 breakpoint: 1600,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
                 }
             },
             {
@@ -27,7 +26,8 @@ $(document).ready(function () {
                     slidesToShow: 1,
                     slidesToScroll: 1
                 }
-            }
+            },
+           
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
